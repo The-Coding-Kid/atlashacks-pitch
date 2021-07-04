d64240cec8871ac7956158b1bc70b30a034aedd6
Veerrohit Veeravdivel <veerrohitv08@gmail.com>
2021-07-04 15:20:28 -0700

    did some test changes to try tsx

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/Header';
 import Steps from './components/Steps';
-import Benefits from './components/Benefits.jsx';
+import Benefits from './components/Benefits';
 import Download from './components/Download';
 import Instructions from './components/Instructions';
 import Team from './components/Team';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -24,4 +23,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
